b7c7029be528caa37321b2c3e2599af58e34e44d
whaticket202344 <132917299+whaticket202344@users.noreply.github.com>
2023-05-08 10:55:52 -0300

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -8,7 +8,6 @@ module.exports = {
   dialect: process.env.DB_DIALECT || "mysql",
   timezone: "-03:00",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
